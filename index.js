e98fe9c25df628597d2fdbc5e82e0ade1b326823
shahriar saleque <shahriar.saleque@tekarsh.com>
2025-06-02 15:06:29 +0600

    start to solve reducer challenges

--- index.js
+++ index.js
@@ -341,3 +341,46 @@ function Field({ label, name, setName}) {
     </label>
   );
 }
+
+// Challenge - 7
+// reducers work 
+
+export default function ContactList({contacts, selectedId, dispatch}) {
+  return (
+    <section className="contact-list">
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.id}>
+            <button
+              onClick={() => {
+                // TODO: dispatch changed_selection
+                dispatch({type: 'changed_selection', contactId: contact.id})
+              }}>
+              {selectedId === contact.id ? <b>{contact.name}</b> : contact.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+import { useState } from 'react';
+
+export default function Chat({contact, message, dispatch}) {
+  return (
+    <section className="chat">
+      <textarea
+        value={message}
+        placeholder={'Chat to ' + contact.name}
+        onChange={(e) => {
+          // TODO: dispatch edited_message
+          // (Read the input value from e.target.value)
+          dispatch({type: 'edited_message', message: e.target.value})
+        }}
+      />
+      <br />
+      <button>Send to {contact.email}</button>
+    </section>
+  );
+}
